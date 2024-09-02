45f445044125fc493fbe13cde88ee6def49dbff2
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-09-02 14:38:55 +0200

    can make a move by click

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
-import Board from "./classes/Board";
+import BoardClass from "./classes/Board";
+import { useState } from "react";
 
 function App() {
-    const board = new Board();
-    return <>{board.render()}</>;
+  const [state, _setState] = useState({
+    board: new BoardClass(() => setState()),
+  });
+
+  const setState = (prop: string = "", value: any = "") => {
+    _setState({ ...state, [prop]: value });
+  };
+
+  if (state.board.checkForWin()) {
+    alert("hej du vann");
+  }
+  console.log(state.board.checkForWin());
+
+  return <>{state.board.render()}</>;
 }
 
 export default App;
