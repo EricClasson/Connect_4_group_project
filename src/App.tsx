59be9cba3ac7733524d9dee89dfc1db87324efbb
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-09-03 11:54:33 +0200

    changed my mind on createplayer and put everything in app instead and rethought when it came to the code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
-import './App.css';
-import BoardClass from './classes/Board';
-import { useState } from 'react';
+import "./App.css";
+import BoardClass from "./classes/Board";
+import { FormEvent, useState } from "react";
+import PlayerClass from "./classes/Player";
 
 function App() {
-    const [state, _setState] = useState({
-        board: new BoardClass(() => setState()),
-    });
+  const [state, _setState] = useState({
+    board: new BoardClass(() => setState()),
+    playerRed: null as PlayerClass | null,
+    playerYellow: null as PlayerClass | null,
+  });
 
-    const setState = (prop: string = '', value: any = '') => {
-        _setState({ ...state, [prop]: value });
-    };
+  const setState = (prop: string = "", value: any = "") => {
+    _setState({ ...state, [prop]: value });
+  };
 
-    if (state.board.checkForWin()) {
-        alert('hej du vann');
-    }
-    console.log(state.board.checkForWin());
+  const { board, playerRed, playerYellow } = state;
 
-    return <>{state.board.render()}</>;
+  function registerName(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const playerRed = form.elements[0] as HTMLInputElement;
+    const playerYellow = form.elements[1] as HTMLInputElement;
+    state.playerRed = new PlayerClass(playerRed.value, "Red");
+    state.playerYellow = new PlayerClass(playerYellow.value, "Yellow");
+    console.log(state.playerYellow);
+    console.log(state.playerRed);
+    board.stateUpdater();
+  }
+
+  const CreatePlayer = () => {
+    return (
+      <form className="modal" onSubmit={registerName}>
+        <h2>change player</h2>
+        <div className="player-selection">
+          <label>Red Player</label>
+          <input
+            type="text"
+            name="playerRed"
+            placeholder="Namn på röd spelare"
+          />
+        </div>
+        <div className="player-selection">
+          <label>Yellow Player</label>
+          <input
+            type="text"
+            name="playerRed"
+            placeholder="Namn på röd spelare"
+          />
+        </div>
+        <button type="submit">Start Game</button>
+      </form>
+    );
+  };
+
+  return <>{!playerRed || !playerYellow ? <CreatePlayer /> : board.render()}</>;
 }
 
 export default App;
