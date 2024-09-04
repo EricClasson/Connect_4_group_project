705f8c3d83d459fd8d81e8c325af6e919bf87e89
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-09-04 10:49:01 +0200

    importing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import './styles/registerCreatePlayer.css';
+
 import BoardClass from './classes/Board';
 import { FormEvent, useState } from 'react';
 import PlayerClass from './classes/Player';
