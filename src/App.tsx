929aa5688d0c1fa315508d4574e6778d6ad6552c
kalle pettersson <kallle.v.pettersson@gmail.com>
2024-09-04 10:50:39 +0200

    i made this classes ass for Ai with the same structure as playerclass

--- src/App.tsx
+++ src/App.tsx
@@ -4,3 +4,15 @@ import './styles/registerCreatePlayer.css';
 import BoardClass from './classes/Board';
 import { FormEvent, useState } from 'react';
 import PlayerClass from './classes/Player';
+
+class EasyAIClass extends PlayerClass {
+  constructor(name: string, color: string) {
+    super(name, color);
+  }
+}
+
+class HardAIClass extends PlayerClass {
+  constructor(name: string, color: string) {
+    super(name, color);
+  }
+}
