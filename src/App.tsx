6a259e6a94a70fa901a081676d3d34f542a19713
BomanStatic <boman.staticsites@gmail.com>
2024-09-06 09:34:35 +0200

    Fixed: styling on starting screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,6 @@ import BoardClass from './classes/Board';
 import { FormEvent, useEffect, useState } from 'react';
 import PlayerClass from './classes/Player';
 
-class EasyAIClass extends PlayerClass {
-  constructor(name: string, color: string) {
-    super(name, color);
-  }
-}
-
-class HardAIClass extends PlayerClass {
-  constructor(name: string, color: string) {
-    super(name, color);
-  }
-}
-
 function App() {
   const COMPUTER_DELAY = 1000;
   const [state, _setState] = useState({
@@ -31,107 +19,122 @@ function App() {
 
   const { board, playerRed, playerYellow } = state;
 
+  function registerName(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const playerRedName = form.elements.namedItem('playerRed') as HTMLInputElement;
+    const playerYellowName = form.elements.namedItem('playerYellow') as HTMLInputElement;
+    const playerYellowOption = form.elements.namedItem('playerYellowOption') as HTMLInputElement;
+    const playerRedOption = form.elements.namedItem('playerRedOption') as HTMLInputElement;
+    console.log(playerYellowOption.value);
+
+    const isNameValid = (name: string) => {
+      return name.trim() !== '' && isNaN(Number(name));
+    };
+
+    // if (!playAgainstRedAiEasy.checked && !isNameValid(playerRedName.value)) {
+    //   alert('Please enter a valid name for the Red player.');
+    //   return;
+    // }
+
+    // if (!playAgainstYellowAiEasy.checked && !isNameValid(playerYellowName.value)) {
+    //   alert('Please enter a valid name for the Yellow player.');
+    //   return;
+    // }
+
+    // if (playAgainstRedAiEasy.checked) {
+    //   state.playerRed = playAgainstRedAiHard.value === 'easy' ? new EasyAIClass('AI Red', 'Red') : new HardAIClass('AI Red', 'Red');
+    // } else {
+    //   state.playerRed = new PlayerClass(playerRedInput.value, 'Red');
+    // }
+
+    // if (playAgainstYellowAiEasy.checked) {
+    //   state.playerYellow =
+    //     playAgainstYellowAiHard.value === 'easy' ? new EasyAIClass('AI Yellow', 'Yellow') : new HardAIClass('AI Yellow', 'Yellow');
+    // } else {
+    //   state.playerYellow = new PlayerClass(playerYellowInput.value, 'Yellow');
+    // }
+    switch (playerRedOption.value) {
+      case 'human':
+        state.playerRed = new PlayerClass(playerRedName.value, 'Red', false, false);
+        break;
+      case 'false':
+        state.playerRed = new PlayerClass(playerRedName.value, 'Red', true, false);
+        break;
+      case 'true':
+        state.playerRed = new PlayerClass(playerRedName.value, 'Red', true, true);
+        break;
+    }
+    switch (playerYellowOption.value) {
+      case 'human':
+        state.playerYellow = new PlayerClass(playerYellowName.value, 'Yellow', false, false);
+        break;
+      case 'false':
+        state.playerYellow = new PlayerClass(playerYellowName.value, 'Yellow', true, false);
+        break;
+      case 'true':
+        state.playerYellow = new PlayerClass(playerYellowName.value, 'Yellow', true, true);
+        break;
+    }
+    console.log(state.playerRed);
+    console.log(state.playerYellow);
+    board.stateUpdater();
+  }
   useEffect(() => {
-    if (
-      playerYellow?.isAI &&
-      board.currentPlayer === 'Yellow' &&
-      !board.gameOver
-    ) {
+    if (playerYellow?.isAI && board.currentPlayer === 'Yellow' && !board.gameOver) {
       setTimeout(() => playerYellow.makeAIMove(board), COMPUTER_DELAY);
     }
     if (playerRed?.isAI && board.currentPlayer === 'Red' && !board.gameOver) {
       setTimeout(() => playerRed.makeAIMove(board), COMPUTER_DELAY);
     }
   }, [setState]);
-
-  function registerName(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const form = event.target as HTMLFormElement;
-    const playerRedInput = form.elements[0] as HTMLInputElement;
-    const playerYellowInput = form.elements[1] as HTMLInputElement;
-    // under this is ai players red and yellow hard ore Easy
-    const playAgainstRedAiEasy = form.elements[2] as HTMLInputElement;
-    const playAgainstRedAiHard = form.elements[3] as HTMLSelectElement;
-    const playAgainstYellowAiEasy = form.elements[4] as HTMLInputElement;
-    const playAgainstYellowAiHard = form.elements[5] as HTMLSelectElement;
-
-    const isNameValid = (name: string) => {
-      return name.trim() !== '' && isNaN(Number(name));
-    };
-
-    if (!playAgainstRedAiEasy.checked && !isNameValid(playerRedInput.value)) {
-      alert('Please enter a valid name for the Red player.');
-      return;
-    }
-
-    if (
-      !playAgainstYellowAiEasy.checked &&
-      !isNameValid(playerYellowInput.value)
-    ) {
-      alert('Please enter a valid name for the Yellow player.');
-      return;
-    }
-
-    if (playAgainstRedAiEasy.checked) {
-      state.playerRed =
-        playAgainstRedAiHard.value === 'easy'
-          ? new EasyAIClass('AI Red', 'Red')
-          : new HardAIClass('AI Red', 'Red');
-    } else {
-      state.playerRed = new PlayerClass(playerRedInput.value, 'Red');
-    }
-
-    if (playAgainstYellowAiEasy.checked) {
-      state.playerYellow =
-        playAgainstYellowAiHard.value === 'easy'
-          ? new EasyAIClass('AI Yellow', 'Yellow')
-          : new HardAIClass('AI Yellow', 'Yellow');
-    } else {
-      state.playerYellow = new PlayerClass(playerYellowInput.value, 'Yellow');
-    }
-
-    console.log(state.playerYellow);
-    console.log(state.playerRed);
-    board.stateUpdater();
-  }
-
   const CreatePlayer = () => {
     return (
       <form className="modal" onSubmit={registerName}>
-        <h2>Change player</h2>
+        <h2>Connect 4</h2>
         <div className="player-selection">
-          <label>Red Player</label>
-          <input
-            type="text"
-            name="playerRed"
-            placeholder="Namn på röd spelare"
-          />
+          <label htmlFor="playerRed">Red Player</label>
+          <input type="text" name="playerRed" placeholder="Namn på röd spelare" />
         </div>
-        <label>
-          <input type="checkbox" name="playAgainstRedAi" /> Play against AI
-        </label>
-        <select name="difficultyRedAi">
-          <option value="easy">Easy</option>
-          <option value="hard">Hard</option>
-        </select>
+        <div className="modal-option">
+          <label htmlFor="humanRed">
+            Human
+            <input type="radio" name="playerRedOption" id="humanRed" value={'human'} />
+          </label>
+          <label htmlFor="aiEasyRed">
+            Ai - Easy
+            <input type="radio" name="playerRedOption" id="aiEasyRed" value={'false'} />
+          </label>
+          <label htmlFor="aiHardRed">
+            Ai - Hard
+            <input type="radio" name="playerRedOption" id="aiHardRed" value={'true'} />
+          </label>
+        </div>
         <div className="player-selection">
           <label>Yellow Player</label>
-          <input
-            type="text"
-            name="playerYellow"
-            placeholder="Namn på gul spelare"
-          />
+          <input type="text" name="playerYellow" placeholder="Namn på gul spelare" />
         </div>
-        <label>
-          <input type="checkbox" name="playAgainstYellowAi" /> Play against AI
-        </label>
-        <select name="difficultyYellowAi">
+        <div className="modal-option">
+          <label htmlFor="humanYellow">
+            Human
+            <input type="radio" name="playerYellowOption" id="humanYellow" value={'human'} />
+          </label>
+          <label htmlFor="aiEasyYellow">
+            Ai - Easy
+            <input type="radio" name="playerYellowOption" id="aiEasyYellow" value={'false'} />
+          </label>
+          <label htmlFor="aiHardYellow">
+            Ai - Hard
+            <input type="radio" name="playerYellowOption" id="aiHardYellow" value={'true'} />
+          </label>
+        </div>
+        {/* <select name="difficultyYellowAi">
           <option value="easy">Easy</option>
           <option value="hard">Hard</option>
-        </select>
-        <section className="center-button">
-          <button type="submit">Start Game</button>
-        </section>
+        </select> */}
+        <button className="btn" type="submit">
+          Start Game
+        </button>
       </form>
     );
   };
@@ -154,30 +157,12 @@ function App() {
 
             <div className="winner-display">
               {' '}
-              <h3
-                className={
-                  state.board.winner === ''
-                    ? ''
-                    : state.board.winner === 'Red'
-                    ? 'red-text'
-                    : 'yellow-text'
-                }
-              >
+              <h3 className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}>
                 {' '}
                 ({state.board.winner})
               </h3>
-              <h3
-                className={
-                  state.board.winner === ''
-                    ? ''
-                    : state.board.winner === 'Red'
-                    ? 'red-text'
-                    : 'yellow-text'
-                }
-              >
-                {state.board.winner === 'Red'
-                  ? playerRed!.name
-                  : playerYellow!.name}
+              <h3 className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}>
+                {state.board.winner === 'Red' ? playerRed!.name : playerYellow!.name}
               </h3>
             </div>
           </>
@@ -201,16 +186,8 @@ function App() {
 
   return (
     <>
-      {!playerRed || !playerYellow ? (
-        <CreatePlayer />
-      ) : (
-        board.render(playerRed, playerYellow)
-      )}
-      {!board.gameOver ? (
-        <div className="game-currentplayer"></div>
-      ) : (
-        <ViewWinner />
-      )}
+      {!playerRed || !playerYellow ? <CreatePlayer /> : board.render(playerRed, playerYellow)}
+      {!board.gameOver ? <div className="game-currentplayer"></div> : <ViewWinner />}
     </>
   );
 }
