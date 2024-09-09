35a99c901754796e1afb503f09cb077faeb54463
BomanStatic <boman.staticsites@gmail.com>
2024-09-09 11:02:38 +0200

    Fixed bug where human vs ai did not work

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,6 @@ function App() {
   };
 
   const { board, playerRed, playerYellow } = state;
-  
-  useEffect(() => {
-    if (playerYellow?.isAI && board.currentPlayer === 'Yellow' && !board.gameOver) {
-      setTimeout(() => playerYellow.makeAIMove(board), COMPUTER_DELAY);
-    }
-    if (playerRed?.isAI && board.currentPlayer === 'Red' && !board.gameOver) {
-      setTimeout(() => playerRed.makeAIMove(board), COMPUTER_DELAY);
-    }
-  }, [setState]);
 
   function registerName(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -84,58 +75,58 @@ function App() {
     if (playerRed?.isAI && board.currentPlayer === 'Red' && !board.gameOver) {
       setTimeout(() => playerRed.makeAIMove(board), COMPUTER_DELAY);
     }
-  }, [setState]);
+  }, [setState, playerRed, playerYellow, board]);
 
   const CreatePlayer = () => {
     return (
-     <>
-      <form className="modal" onSubmit={registerName}>
-        <h2>Connect Four</h2>
-        <div className="player-selection player-red">
-          <label htmlFor="playerRed">Red Player</label>
-          <input type="text" name="playerRed" placeholder="Name for red player" />
-          <div className="modal-option">
-            <label htmlFor="humanRed">
-              Human
-              <input type="radio" defaultChecked name="playerRedOption" id="humanRed" value={'human'} />
-            </label>
-            <label htmlFor="aiEasyRed">
-              Ai - Easy
-              <input type="radio" name="playerRedOption" id="aiEasyRed" value={'false'} />
-            </label>
-            <label htmlFor="aiHardRed">
-              Ai - Hard
-              <input type="radio" name="playerRedOption" id="aiHardRed" value={'true'} />
-            </label>
+      <>
+        <form className="modal" onSubmit={registerName}>
+          <h2>Connect Four</h2>
+          <div className="player-selection player-red">
+            <label htmlFor="playerRed">Red Player</label>
+            <input type="text" name="playerRed" placeholder="Name for red player" />
+            <div className="modal-option">
+              <label htmlFor="humanRed">
+                Human
+                <input type="radio" defaultChecked name="playerRedOption" id="humanRed" value={'human'} />
+              </label>
+              <label htmlFor="aiEasyRed">
+                Ai - Easy
+                <input type="radio" name="playerRedOption" id="aiEasyRed" value={'false'} />
+              </label>
+              <label htmlFor="aiHardRed">
+                Ai - Hard
+                <input type="radio" name="playerRedOption" id="aiHardRed" value={'true'} />
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="player-selection player-yellow">
-          <label>Yellow Player</label>
-          <input type="text" name="playerYellow" placeholder="Name for yellow player" />
-          <div className="modal-option">
-            <label htmlFor="humanYellow">
-              Human
-              <input type="radio" defaultChecked name="playerYellowOption" id="humanYellow" value={'human'} />
-            </label>
-            <label htmlFor="aiEasyYellow">
-              Ai - Easy
-              <input type="radio" name="playerYellowOption" id="aiEasyYellow" value={'false'} />
-            </label>
-            <label htmlFor="aiHardYellow">
-              Ai - Hard
-              <input type="radio" name="playerYellowOption" id="aiHardYellow" value={'true'} />
-            </label>
+          <div className="player-selection player-yellow">
+            <label>Yellow Player</label>
+            <input type="text" name="playerYellow" placeholder="Name for yellow player" />
+            <div className="modal-option">
+              <label htmlFor="humanYellow">
+                Human
+                <input type="radio" defaultChecked name="playerYellowOption" id="humanYellow" value={'human'} />
+              </label>
+              <label htmlFor="aiEasyYellow">
+                Ai - Easy
+                <input type="radio" name="playerYellowOption" id="aiEasyYellow" value={'false'} />
+              </label>
+              <label htmlFor="aiHardYellow">
+                Ai - Hard
+                <input type="radio" name="playerYellowOption" id="aiHardYellow" value={'true'} />
+              </label>
+            </div>
           </div>
-        </div>
-        <button className="btn" type="submit">
-          Start Game
-        </button>
-      </form>
-         <ViewHighScoreList />
-     </>
+          <button className="btn" type="submit">
+            Start Game
+          </button>
+        </form>
+        <ViewHighScoreList />
+      </>
     );
   };
-  
+
   const highScore = (name: string, moves: number) => {
     const highscores = JSON.parse(localStorage.getItem('highscores') || '[]') as { name: string; moves: number }[];
 
@@ -203,11 +194,15 @@ function App() {
 
             <div className="winner-display">
               {' '}
-              <h3 className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}>
+              <h3
+                className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}
+              >
                 {' '}
                 {state.board.winner}
               </h3>
-              <h3 className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}>
+              <h3
+                className={state.board.winner === '' ? '' : state.board.winner === 'Red' ? 'red-text' : 'yellow-text'}
+              >
                 {state.board.winner === 'Red' ? playerRed!.name : playerYellow!.name}
               </h3>
             </div>
